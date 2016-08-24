210e9168bba8f3503fde8c5ae3de5f319574ee12
Hariprasad Bandaru <orclhariprasad@gmail.com>
2016-08-24 11:31:28 +0530

    First Commit

---
+++ app/app.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+@Component({
+  selector: 'my-app',
+  templateUrl : `app/home/dashboard.html`,
+})
+export class AppComponent {
+  title = 'Carousel'; 
+}
